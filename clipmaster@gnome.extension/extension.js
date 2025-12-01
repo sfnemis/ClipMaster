1e4ca54cfff176060343db4f6a86ec7edcc41928
sfnemis <sfnemis@gmail.com>
2025-12-01 08:04:58 +0300

    Prepare for GNOME Extensions submission
    - Add GPL-2.0-or-later license headers to all source files
    - Add LICENSE file to extension directory
    - Update metadata.json with correct GitHub URL and donations
    - Improve extension description
    - Create EGO submission package (shell-extension.zip)
    
    Ready for extensions.gnome.org submission

--- clipmaster@gnome.extension/extension.js
+++ clipmaster@gnome.extension/extension.js
@@ -2,15 +2,22 @@
  * ClipMaster - GNOME Shell Extension
  * A powerful clipboard manager with history, favorites, lists, and more.
  * 
- * Features:
- * - Clipboard history with configurable size
- * - Image and file support
- * - Favorites and custom lists
- * - Search functionality
- * - Edit items, add titles
- * - Plain text paste option
- * - Export/Import
- * - Keyboard shortcuts
+ * Copyright (C) 2025 SFN <sfnemis@github.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
  */
 
 import GLib from 'gi://GLib';
